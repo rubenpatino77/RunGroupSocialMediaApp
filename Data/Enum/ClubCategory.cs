b83a717d72517ad8eb388814e97835fd482a5b83
rubenpatino77 <rubenpatino77@gmail.com>
2023-08-22 11:39:49 -0700

    Initial setup of project and model objects.

---
+++ Data/Enum/ClubCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace RunGroupSocialMedia.Data.Enum
+{
+	public enum ClubCategory
+	{
+        RoadRunner,
+        Womens,
+        City,
+        Trail,
+        Endurance
+    }
+}
+
