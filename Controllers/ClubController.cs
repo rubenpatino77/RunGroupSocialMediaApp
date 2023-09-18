6c1fd72bf398e274b69b2e09b7319f96392b2ecc
rubenpatino77 <rubenpatino77@gmail.com>
2023-09-18 15:39:58 -0700

    Connect Club controller to Azure Blob Storage and add Create button to Club and Race Index pages.

--- Controllers/ClubController.cs
+++ Controllers/ClubController.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RunGroupSocialMedia.Data;
 using RunGroupSocialMedia.Interfaces;
 using RunGroupSocialMedia.Models;
+using RunGroupSocialMedia.Services;
+using RunGroupSocialMedia.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,10 +18,12 @@ namespace RunGroupSocialMedia.Controllers
     public class ClubController : Controller
     {
         private readonly IClubRepository _clubRepository;
+        private readonly IPhotoService _photoService;
 
-        public ClubController(IClubRepository clubRepository)
+        public ClubController(IClubRepository clubRepository, IPhotoService photoService)
         {
             _clubRepository = clubRepository;
+            _photoService = photoService;
         }
 
         // GET: /<controller>/
@@ -40,15 +45,46 @@ namespace RunGroupSocialMedia.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Club club)
+        public async Task<IActionResult> Create(CreateClubViewModel form)
         {
-            if (!ModelState.IsValid)
+            Club club = null;
+            if (ModelState.IsValid)
             {
-                return View(club);
+                club = uploadClubAndImageAsync(form).Result;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Photo upload failed.");
             }
 
+            return View(club);
+        }
+
+        public async Task<Club> uploadClubAndImageAsync(CreateClubViewModel form)
+        {
+            Club club = null;
+            string path = await _photoService.ConvertIFormFileToStringPathAsync(form.photo.ImageFile);
+            string fileName = form.photo.ImageFile.FileName;
+
+            BlobServiceClient blobServiceClient = null; // Initialize as null
+            _photoService.GetBlobServiceClientSAS(ref blobServiceClient);
+
+            _photoService.UploadFromFileAsync(blobServiceClient.GetBlobContainerClient("run-group-container"), path, fileName);
+
+            var photoUrl = blobServiceClient.Uri.AbsoluteUri;
+            club = new Club
+            {
+                Title = form.Title,
+                Description = form.Description,
+                Image = "https://rungroup.blob.core.windows.net/run-group-container/" + fileName + "?" + _photoService.sasToken,
+                Address = form.Address,
+                ClubCategory = form.ClubCategory
+
+            };
             _clubRepository.Add(club);
-            return RedirectToAction("Index");
+
+            return club;
         }
     }
 }
--- ViewModels/CreateClubViewModel.cs
+++ ViewModels/CreateClubViewModel.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using RunGroupSocialMedia.Data.Enum;
+using RunGroupSocialMedia.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace RunGroupSocialMedia.ViewModels
 {
 	public class CreateClubViewModel
 	{
-		
-	}
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public Address? Address { get; set; }
+        public ClubCategory ClubCategory { get; set; }
+
+        public Photo photo { get; set; }
+    }
 }
 
